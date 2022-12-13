a263e8a6d24bcc2f2f27307511a6dc880730494d
Navindi <navindi52@gmail.com>
2022-12-13 16:34:46 +0530

    Update lib\main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Login Page',
+      title: 'Flutter Login Page ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
